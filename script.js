b0ec8d676d24b5480697d1a87a9047508af0e331
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-19 13:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 
-    
+    const flipImage = document.getElementById("flip");
+
+    flipImage.addEventListener("mouseover", () => {
+      flipImage.src = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+    });
+
+    flipImage.addEventListener("mouseout", () => {
+      flipImage.src = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+    });
