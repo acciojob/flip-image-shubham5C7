00d7d61aec52f68197f52f7c11efe66337b72e72
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-19 13:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const image = document.getElementById("flip");
 
-const img = document.getElementById("flip");
-
-  img.addEventListener("mouseover", () => {
-    img.src = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+  image.addEventListener("mouseover",()=>{
+     image.src = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
   });
 
-  img.addEventListener("mouseout", () => {
-    img.src = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
-  });
+   image.addEventListener("mouseout", () => {
+       image.src = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+    });
+    
