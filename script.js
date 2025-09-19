8a71497fa8766ba3618314ee8c95c5cc8052e7ee
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-19 11:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// write js code if required
- const img = document.getElementById("flip");
-  
-   img.addEventListener("mouseover",()=>{
-    img.src ="https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
-   });
 
-   img.addEventListener("mouseout",()=>{
-    img.src="https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
-   });
