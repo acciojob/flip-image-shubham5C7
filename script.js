a8be6eff4668060cf65194615744061aaf0963ca
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-19 13:54:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-const image = document.getElementById("flip");
 
-  image.addEventListener("mouseover",()=>{
-     image.src = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
-  });
-
-   image.addEventListener("mouseout", () => {
-       image.src = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
-    });
     
